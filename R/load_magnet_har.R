a37844399b5094759bae844e1a1ff6a71413c4cd
Thijs de Lange <thijs.delange@wur.nl>
2021-12-22 20:40:14 +0100

    - packages are added, including roxygen statement for "%>%" from magrittr - MTED-HARr package does not work, is now loaded in DESCRIPTION with Remotes

---
+++ R/load_magnet_har.R
@@ -0,0 +1,29 @@
+#' @importFrom magrittr %>%
+
+extract_MAGNET_d1 <- function(scenarios, periods, indicators, filetype, filepath) {
+  d1_scenarios_all <- NULL
+  for (k in 1:length(scenarios)) {
+    d1_periods_all <- NULL
+    for (l in 1:length(periods)) {
+      d1a <- 'MTED-HARr'::read_har(file.path(filepath,paste0("",scenarios[k],"_",periods[l],"_",filetype,".har")))
+
+      d1_indicators_all <- NULL
+      for (m in 1:length(indicators)) {
+        d1 <- d1a[[indicators[m]]]
+        d1_indicators <- d1 %>%
+          data.frame() %>%
+          dplyr::rename("value" = ".") %>%
+          dplyr::mutate(region = row.names(.)) %>%
+          dplyr::mutate(indicator = indicators[m]) %>%
+          dplyr::mutate(scenario = scenarios[k]) %>%
+          dplyr::mutate(year = as.numeric(substr(periods[l], 6, 9)))
+
+
+        d1_indicators_all <- rbind(d1_indicators_all, d1_indicators)
+      }
+      d1_periods_all <- rbind(d1_periods_all, d1_indicators_all)
+    }
+    d1_scenarios_all <- rbind(d1_scenarios_all, d1_periods_all)
+  }
+  print(d1_scenarios_all)
+}
--- tests/testthat/test-load_magnet_har.R
+++ tests/testthat/test-load_magnet_har.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+# Create new folder for relevant files
+raw_path <- "W:/WECR/Magnet_data9/PCSL/4_MAGNET"
+
+dataBasePath <- file.path(raw_path,'Basedata')
+dataUpdatesPath <- file.path(raw_path,'Updates')
+dataSolPath <- file.path(raw_path,'Solutions')
+dataAddPath <- file.path(raw_path,'Additional')
+#dataShocksPath <- "./4_MAGNET/Shocks"
+dataResultPath <- file.path(raw_path,'Results')
+
+scenarios<-c("Base_PCSL_ProdPath_YouthBranch", "Base_PCSL_ProdPath", "Base_PCSL_Extpath")
+periods <- c("2014-2020", "2020-2025", "2025-2030", "2030-2035", "2035-2040", "2040-2045")
+
+
+extract_MAGNET_d1(scenarios, periods, "POP", "update", dataUpdatesPath)
