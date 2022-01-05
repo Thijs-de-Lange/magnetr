b2419fc2520e45722dc0ee464b41d070170e9aac
Thijs de Lange <thijs.delange@wur.nl>
2022-01-05 21:24:30 +0100

    - description is added - variables within function are added to Utils::globalvariables()

--- R/load_magnet_har.R
+++ R/load_magnet_har.R
@@ -1,12 +1,33 @@
-#' @importFrom magrittr %>%
+#'@title Load MAGNET results in long format in R
+#'
+#'
+#'@param scenarios The scenario name in quotation marks or a vector of scenario names
+#'@param periods The period name in quotation marks or a vector of the period names
+#'@param indicators The indicator name quotation marks or a vector of the indicator names, indicator name(s) need to correspond with the Header names in the .har file
+#'@param region_select The region name quotation marks or a vector of the region names, region name(s) corresponds with abbreviations used in MAGNET
+#'@param filetype This corresponds with from where the results are extracted, like from a .har or .sol file with the name "Update", "Solution" or "update_view"
+#'@param file_path The file path where the MAGNET results in e.g. .har or .sol files are storred
+#'@param file_suffix The suffix of the data file e.g. har or sol
+#'@examples
+#'
+#'\dontrun{
+#'
+#'raw_path <- "W:/WECR/Magnet_data9/PCSL/4_MAGNET"/"Updates"
+#'scenarios <-c("Base_PCSL_ProdPath_YouthBranch" , "Base_PCSL_ProdPath", "Base_PCSL_Extpath")
+#'periods <- c("2014-2020", "2020-2025", "2025-2030", "2030-2035", "2035-2040", "2040-2045")
+#'country <- c("Ken")
 
+#'extract_MAGNET_d1(scenarios, periods, "POP", country,"update", dataUpdatesPath, "har")
+#'}
+#'
+#' @importFrom magrittr %>%
 #' @export
-extract_MAGNET_d1 <- function(scenarios, periods, indicators, region_select, filetype, filepath, fileformat) {
+extract_MAGNET_d1 <- function(scenarios, periods, indicators, region_select, filetype, file_path, file_suffix) {
   d1_scenarios_all <- NULL
   for (k in 1:length(scenarios)) {
     d1_periods_all <- NULL
     for (l in 1:length(periods)) {
-      d1a <- HARr::read_har(file.path(filepath,paste0("",scenarios[k],"_",periods[l],"_",filetype,".",fileformat,"")))
+      d1a <- HARr::read_har(file.path(file_path,paste0("",scenarios[k],"_",periods[l],"_",filetype,".",file_suffix,"")))
 
       d1_indicators_all <- NULL
       for (m in 1:length(indicators)) {
@@ -134,10 +155,10 @@ extract_MAGNET_d1 <- function(scenarios, periods, indicators, region_select, fil
 
 
 #' @export
-extract_MAGNET_base <- function(scenarios, indicators, region_select, base_year, filetype, filepath) { #add year
+extract_MAGNET_base <- function(scenarios, indicators, region_select, base_year, filetype, file_path) { #add year
   d1_scenarios_all <- NULL
   for (k in 1:length(scenarios)) {
-      d1a <- HARr::read_har(file.path(filepath,paste0("BaseData_b_",filetype,".har")))
+      d1a <- HARr::read_har(file.path(file_path,paste0("BaseData_b_",filetype,".har")))
 
       d1_indicators_all <- NULL
       for (m in 1:length(indicators)) {
@@ -168,7 +189,7 @@ extract_MAGNET_base <- function(scenarios, indicators, region_select, base_year,
             dplyr::rename(region = paste0("",names_col2,"")) %>%
             dplyr::mutate(indicator = indicators[m]) %>%
             dplyr::mutate(scenario = scenarios[k]) %>%
-            mutate(year = base_year) %>%
+            dplyr::mutate(year = base_year) %>%
             dplyr::mutate(variable_name =  names(attributes(d1)$dim[1]))
 
           d1_indicators_all <- rbind(d1_indicators_all, d1_indicators)
@@ -264,51 +285,51 @@ extract_MAGNET_base <- function(scenarios, indicators, region_select, base_year,
 
 
 #' @export
-magnet_indicator <- function(indicator, scenarios, periods, base_year, region_select, filepath_base, filepath_scenario){
+magnet_indicator <- function(indicator, scenarios, periods, base_year, region_select, file_path_base, file_path_scenario){
 
 
   if(indicator == "population" | indicator == "gdp_pc") {
-    population <- rbind(extract_MAGNET_base(scenarios, "POP", region_select, base_year, "all", filepath_base),
-                 extract_MAGNET_d1(scenarios, periods, "POP", region_select, "Update", filepath_scenario, "har"))
+    population <- rbind(extract_MAGNET_base(scenarios, "POP", region_select, base_year, "all", file_path_base),
+                 extract_MAGNET_d1(scenarios, periods, "POP", region_select, "Update", file_path_scenario, "har"))
 
     if(indicator == "population") {
       print(population) }
 
   } else if (indicator == "labour") {
-    labour <- rbind(extract_MAGNET_base(scenarios, "QLAB", region_select, base_year, "all", filepath_base),
-                 extract_MAGNET_d1(scenarios, periods, "QLAB", region_select, "Update", filepath_scenario, "har"))
+    labour <- rbind(extract_MAGNET_base(scenarios, "QLAB", region_select, base_year, "all", file_path_base),
+                 extract_MAGNET_d1(scenarios, periods, "QLAB", region_select, "Update", file_path_scenario, "har"))
 
     print(labour)
 
   } else if (indicator == "nutrient_cons_pc") {
-    nutrient_cons_pc <- rbind(extract_MAGNET_base(scenarios, "NSPC", region_select, base_year, "all", filepath_base),
-                    extract_MAGNET_d1(scenarios, periods, "NSPC", region_select, "update_view", filepath_scenario, "har"))
+    nutrient_cons_pc <- rbind(extract_MAGNET_base(scenarios, "NSPC", region_select, base_year, "all", file_path_base),
+                    extract_MAGNET_d1(scenarios, periods, "NSPC", region_select, "update_view", file_path_scenario, "har"))
 
     print(nutrient_cons_pc)
 
   } else if (indicator == "gdp" | indicator == "gdp_pc") {
-    gdp_base <- extract_MAGNET_base(scenarios, "AG02", region_select, base_year, "all", filepath_base) %>%
+    gdp_base <- extract_MAGNET_base(scenarios, "AG02", region_select, base_year, "all", file_path_base) %>%
       dplyr::rename(commodity1 = region,
              region = commodity) %>%
       dplyr::rename(commodity = commodity1) %>%
       dplyr::group_by(region, indicator, scenario, year) %>%
-      dplyr::summarize(across(c(value),
+      dplyr::summarize(dplyr::across(c(value),
                        sum,
                        na.rm = TRUE),
                 .groups = "drop"
       ) %>%
-      rename(value_base = value)
+      dplyr::rename(value_base = value)
 
-    qgdp <- extract_MAGNET_d1(scenarios, periods, "qgdp", region_select, "Solution", filepath_scenario, "sol") %>%
+    qgdp <- extract_MAGNET_d1(scenarios, periods, "qgdp", region_select, "Solution", file_path_scenario, "sol") %>%
       dplyr::rename(q = value) %>%
       dplyr::select(-indicator) %>%
       dplyr::mutate(year = as.character(year))
 
     gdp <- dplyr::left_join(gdp_base %>%
-                       select(-year)
+                              dplyr::select(-year)
                      , qgdp) %>%
       rbind(., gdp_base %>%
-              mutate(q = 0)
+              dplyr::mutate(q = 0)
       ) %>%
       dplyr::group_by(indicator, region, scenario) %>%
       dplyr::arrange(year) %>%
@@ -327,3 +348,5 @@ magnet_indicator <- function(indicator, scenarios, periods, base_year, region_se
   }
 }
 
+
+utils::globalVariables(c(".", "commodity", "commodity1", "variable1", "region", "year", "scenario", "value", "value_base", "percent_cumulative"))
